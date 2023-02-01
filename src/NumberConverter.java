0554fd91ff70d03ed0dcdca822e487e482e5313d
hansun <hsun7920@bths.edu>
2023-02-01 00:23:01 -0500

    stuff

--- src/NumberConverter.java
+++ src/NumberConverter.java
@@ -1,6 +1,7 @@
 public class NumberConverter {
     int[] digits;
     int base;
+    char[] numsAndLettersForConv = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'};;
 
     public NumberConverter(int number, int base) {
         String numberAsString = Integer.toString(number);
@@ -26,7 +27,9 @@ public class NumberConverter {
         return digits;
     }
 
-    public int[] convertToDecimal() {
+    public
+
+    public int[] convertToDecimal(int convertBase) {
         int position;
         String convertedOct = "";
         int temp = Integer.parseInt(displayOriginalNumber());
@@ -42,17 +45,18 @@ public class NumberConverter {
         }
     }
 
+    /**
     public String convertToBinary() {
         String convertedBi = "";
         int temp = Integer.parseInt(displayOriginalNumber());
         int i = 0;
         int[] Bi = {0, 1};
-        if(base == 10) {
-        while(temp > 0){
+        if (base == 10) {
+            while (temp > 0) {
                 convertedBi = Bi[temp % 2 + 1] + convertedBi;
                 temp = temp / 2;
             }
-        return convertedBi;
+            return convertedBi;
         }
         if (base == 2) {
             return null;
@@ -80,11 +84,18 @@ public class NumberConverter {
         char[] hexadecimal = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F'};
 
     }
-
-    public String convertEverything() {
-        char[] all = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'};
-
+**/
+    public String convertEverything(int convertBase) {
+        int original = Integer.parseInt(displayOriginalNumber());
+        int toThePowerOf = 0;
+        if(base != 10){
+            original = convertToDecimal(base);
+        }
     }
 }
 
+// make sure if the base is less than 9, while converting, some numbers cannot be converted. like base 8 the number 68 is not gonna convert.
+//if the value in the ones place is bigger than the base, then the conversion does not exist. assuming that it is converting from decimal
+// use Math.pow to see how many times the number has to be multiplied, suggest to remove the number in the ones place, then reverse the num, then just * by the conversion base again
+// ex: base 16   BB3 = 11 * 16 * 16 + 11*16 + 3
 //char[] all = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J' 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '+', '/'};
